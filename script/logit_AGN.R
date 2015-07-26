67ac9b3f22cfdc05958c6a3d901fe0a14648cd74
Rafael <rafael.2706@gmail.com>
2015-07-25 23:43:09 -0300

    3D

--- script/logit_AGN.R
+++ script/logit_AGN.R
@@ -27,3 +27,37 @@ ROCF$class[which(ROCF$class<coords(F1,x="best")[1])]<-0
 confusionMatrix(ROCF$True, ROCF$class)
 
 
+
+new.data <- expand.grid(Xmol = seq(1e-4,2e-1, length = 5000), 
+                        Z = mean(Biffi_data$Z))
+x <-range(AGN_data$log10.NII.Ha.)
+x <- seq(x[1], x[2], length.out=50)    
+y <- range(AGN_data$log10.EW.Ha..)
+y <- seq(y[1], y[2], length.out=50)
+
+z <- outer(x,y, 
+           function(log10.NII.Ha.,log10.EW.Ha..)
+             predict(fit, data.frame(log10.NII.Ha.,log10.EW.Ha..),type = 'response'))
+library(rsm)
+library(lattice)
+YlOrBr <- c("#FFFFD4", "#FED98E", "#FE9929", "#D95F0E", "#993404")
+p<-persp(x,y,z, theta=150, phi=20, 
+         expand = 0.5,shade = 0.1,
+         xlab="Z", ylab=expression(NII.Ha), zlab=expression(log10.EW.Ha),ticktype='detailed',
+         col = YlOrBr,border=NA,xlog=T,ylog=T)
+cor = topo.colors(200)
+
+cairo_pdf("logit3D.pdf")
+trellis.par.set("axis.line",list(axis.text=list(cex=20),col=NA,lty=1,lwd=2))
+par(mar=c(1,1,1,1))
+wireframe(z~x+y,data=data.frame(x=x, y=rep(y, each=length(x)), z=z),
+          par.settings = list(regions=list(alpha=0.4)),
+          col.regions =cor,drape=T,light.source = c(5,5,5),colorkey = FALSE,
+          xlab=list(label=expression(log10.NII.Ha.),cex=1.25), ylab=list(label=expression(log10.EW.Ha..),cex=1.25), 
+          zlab=list(rot=90,label=expression(pi),cex=1.25,dist=-1,rot=0),
+          scale=list(tck=0.75,arrows=FALSE,distance =c(0.75, 0.75, 0.75)))
+
+
+dev.off() 
+
+
